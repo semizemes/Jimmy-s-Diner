dc990a85ce58399b50cfb3b0601db3505468a459
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-12-02 21:44:09 +0500

    feat: refactor code structure for improved readability and maintainability

---
+++ index.js
@@ -0,0 +1,17 @@
+import menuArray from "./data.js";
+
+const menu = document.getElementById('menu');
+
+function getHtml (){
+    return menuArray.map(item => `
+            <div class="item" id="item-${item.id}">
+                <div class="item-img">${item.emoji}</div>
+                <h2 class="item-name">${item.name}</h2>
+                <p class="ingredients">${item.ingredients.join(", ")}</p>
+                <h3 class="item-price">${item.price}$</h3>
+                <button class="add" id="add-${item.id}">+</button>
+            </div>
+        `)
+}
+
+menu.innerHTML = getHtml()
