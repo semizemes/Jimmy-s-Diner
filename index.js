d2bd1756f5c4b1e21ba16f6db76ce927a2736127
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-12-07 01:01:51 +0000

    feat: update order display to show item quantities and improve list styling

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const total = document.getElementById("total");
 const pizza = document.getElementById("pizza");
 const beer = document.getElementById("beer");
 const hamburger = document.getElementById("hamburger");
+const pizzaCountSpan = document.getElementById("pizza-count-span");
+const beerCountSpan = document.getElementById("beer-count-span");
+const hamburgerCountSpan = document.getElementById("hamburger-count-span");
+
 
 const orderedArr = [];
 
@@ -48,32 +52,23 @@ function yourOrderHtml() {
   const pizzaCount = orderedArr.filter((x) => x == 0).length;
   const hamburgerCount = orderedArr.filter((x) => x == 1).length;
   const beerCount = orderedArr.filter((x) => x == 2).length;
-  ordered.innerHTML = `
-        <li class="order-list pizza" id="pizza">
-          <span class="chosen-order-name">Pizza</span>
-          <span>x ${pizzaCount}</span>
-          <button class="remove-btn" data-remove="order0">remove</button>
-          <span class="chosen-order-price">$${14 * pizzaCount}</span>
-        </li>
-        <li class="order-list hamburger" id="hamburger">
-          <span class="chosen-order-name">Hamburger</span>
-          <span>x ${hamburgerCount}</span>
-          <button class="remove-btn" data-remove="order1">remove</button>
-          <span class="chosen-order-price">$${hamburgerCount * 12}</span>
-        </li>
-        <li class="order-list beer" id="beer">
-          <span class="chosen-order-name">Beer</span>
-          <span>x ${beerCount}</span>
-          <button class="remove-btn" data-remove="order2">remove</button>
-          <span class="chosen-order-price">$${beerCount * 12}</span>
-        </li>        
-    `;
+  
   return [pizzaCount, hamburgerCount, beerCount];
 }
 
 function displayOrder(countArr) {
+  console.log(countArr);
+  
   if (countArr[0] > 0) {
     console.log(countArr[0]);
-    pizza.style.display = "block";
+    pizza.style.display = "list-item";
+  }
+  if (countArr[1] > 0) {
+    console.log(countArr[1]);
+    hamburger.style.display = "list-item";
+  }
+  if (countArr[2] > 0) {
+    console.log(countArr[2]);
+    beer.style.display = "list-item";
   }
 }
