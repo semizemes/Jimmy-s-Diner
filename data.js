68012f681cbf5df18b0c4b177ee8d955daa4795c
Bill Croteau <bill@tunko.com>
2025-12-08 15:12:20 -0500

    Fix the ingredients for beer

--- data.js
+++ data.js
@@ -1,25 +1,25 @@
 const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
-        price: 14,
-        emoji: "🍕"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 1
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
-        id: 2
-    }
-]
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    id: 0,
+    price: 14,
+    emoji: "🍕",
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 1,
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain", "hops", "yeast", "water"],
+    price: 12,
+    emoji: "🍺",
+    id: 2,
+  },
+];
 
-export default menuArray
+export default menuArray;
